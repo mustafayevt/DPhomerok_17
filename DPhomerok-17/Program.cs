cb27cd0fcebaa67a49d45e76b4b89dc3d17d5cb5
Tural Mustafayev <mustafayevt@users.noreply.github.com>
2018-12-18 00:36:11 +0400

    edit1

--- DPhomerok-17/Program.cs
+++ DPhomerok-17/Program.cs
@@ -2,14 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace DPhomerok_17
+namespace DP
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
+    interface IUnit
+    {
+        string type { get; set; }
+    }
+    class Solider : IUnit
+    {
+        public string type { get; set; } = "Solider";
+    }
+    class Tank : IUnit
+    {
+        public string type { get; set; } = "Tank";
+    }
+    class Helicopter : IUnit
+    {
+        public string type { get; set; } = "Helicopter";
+    }
+    interface IUnitFactory
+    {
+        IUnit createUnit();
+    }
+
+    interface IStage
+    {
+        List<IUnit> units { get; set; }
+    }
+
 }
